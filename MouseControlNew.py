0d23da6e151ce0e808e1e7e5a7ae6830ecd20f4f
Jonathan Li <jonathanli668899@gmail.com>
2025-04-18 16:04:34 -0400

    e

--- MouseControlNew.py
+++ MouseControlNew.py
@@ -8,69 +8,77 @@ drawing_utils = mp.solutions.drawing_utils
 screen_width, screen_height = pyautogui.size()
 index_y_prev, index_x_prev = 0, 0
 smoothing_factor = 0.7
-click_threshold = 25
-click_delay = 0.1 
-movement_threshold = 50
+click_threshold = 50
+click_delay = 0.1
+movement_threshold = 70 #px
+
+landmark_drawing_spec = drawing_utils.DrawingSpec(color=(255, 0, 0), thickness=2, circle_radius=2)
 
 while True:
+
     ret, frame = cap.read()
     frame = cv2.flip(frame, 1)
     frame_height, frame_width, ret = frame.shape
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
     output = hand_detector.process(rgb_frame)
     hands = output.multi_hand_landmarks
+
     if hands:
         for hand in hands:
-            drawing_utils.draw_landmarks(frame, hand)
+
+            drawing_utils.draw_landmarks(frame, hand, landmark_drawing_spec = landmark_drawing_spec)
             landmarks = hand.landmark
-            index_finger_tip = None
+
+            index_tip = None
             thumb_tip = None
+            middle_tip = None
 
             for id, landmark in enumerate(landmarks):
                 x = int(landmark.x * frame_width)
                 y = int(landmark.y * frame_height)
 
-                if id == 8:  # index finger
+                if id == 8:  # INDEX FINGER
                     cv2.circle(img=frame, center=(x, y), radius = 20, color=(0, 255, 0))
                     index_x_raw = screen_width / frame_width * x
                     index_y_raw = screen_height / frame_height * y
 
-                    # smoothing
+                    # SMOOTHING
                     index_x = int((1 - smoothing_factor) * index_x_raw + smoothing_factor * index_x_prev)
                     index_y = int((1 - smoothing_factor) * index_y_raw + smoothing_factor * index_y_prev)
                     pyautogui.moveTo(index_x, index_y)
                     index_x_prev, index_y_prev = index_x, index_y
-                    index_finger_tip = (index_x_raw, index_y_raw)
+                    index_tip = (index_x_raw, index_y_raw)
 
-                if id == 4:  # thumb
-                    cv2.circle(img=frame, center=(x, y), radius = 30, color = (0, 255, 255))
+                if id == 4:  # THUMB
+                    cv2.circle(img=frame, center=(x, y), radius = 30, color = (0, 255, 0))
                     thumb_x = screen_width / frame_width * x
                     thumb_y = screen_height / frame_height * y
                     thumb_tip = (thumb_x, thumb_y)
 
-                if id == 12: # middle finger
-                    cv2.circle(img=frame, center=(x, y), radius = 30, color = (0, 255, 255))
+                if id == 12: # MIDDLE FINGER
+                    cv2.circle(img=frame, center=(x, y), radius = 30, color = (0, 255, 0))
                     middle_x = screen_width / frame_width * x
                     middle_y = screen_height / frame_height * y
-                    middle_tip = (thumb_x, thumb_y)
+                    middle_tip = (middle_x, middle_y)
                     
 
             # Left click if index and thumb pinch
-            if index_finger_tip and thumb_tip:
-                distance = abs(index_finger_tip[1] - thumb_tip[1])
-                print('Distance btwn thumb & index finger:', distance)
+            if index_tip and thumb_tip:
+                distance = abs(index_tip[1] - thumb_tip[1])
+                print('Left click, :', distance)
                 if distance < click_threshold:
                     pyautogui.click()
                     pyautogui.sleep(click_delay)
 
 
-            # right click if middle and thumb pinch
-            if middle_tip and thumb_tip:
-                distance = abs(middle_tip[1] - thumb_tip[1])
-                print('Distance btwn thumb & middle finger:', distance)
-                if distance < click_threshold:
+            # right click if middle and index touch
+            if middle_tip and index_tip:
+                distance = abs(middle_tip[1] - index_tip[1])
+                if (distance < click_threshold):
+                    print('Right click')
                     pyautogui.rightClick()
                     pyautogui.sleep(click_delay)
+
 
     cv2.imshow('frame', frame)
 
