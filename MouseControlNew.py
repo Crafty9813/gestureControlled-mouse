d03179d919fb9b36346824da3c3cc5e2a1ea7291
Jonathan Li <jonathanli668899@gmail.com>
2025-04-19 15:25:52 -0400

    has false positives

--- MouseControlNew.py
+++ MouseControlNew.py
@@ -8,7 +8,7 @@ drawing_utils = mp.solutions.drawing_utils
 screen_width, screen_height = pyautogui.size()
 index_y_prev, index_x_prev = 0, 0
 smoothing_factor = 0.5
-click_threshold = 50
+click_threshold = 30
 click_delay = 1
 movement_threshold = 70 #px
 
@@ -17,6 +17,7 @@ landmark_drawing_spec = drawing_utils.DrawingSpec(color=(255, 0, 0), thickness=2
 while True:
 
     ret, frame = cap.read()
+    #frame.flags.writeable = False
     frame = cv2.flip(frame, 1) # otherwise left and right movement would be opposite
     frame_height, frame_width, ret = frame.shape
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -60,13 +61,19 @@ while True:
                     middle_x = screen_width / frame_width * x
                     middle_y = screen_height / frame_height * y
                     middle_tip = (middle_x, middle_y)
+
+                if id == 20: #PINKY
+                    cv2.circle(img=frame, center=(x, y), radius = 30, color = (0, 255, 0))
+                    pinky_x = screen_width / frame_width * x
+                    pinky_y = screen_height / frame_height * y
+                    pinky_tip = (pinky_x, pinky_y)
                     
 
             # Left click if index and thumb pinch
             if index_tip and thumb_tip:
                 distance = abs(index_tip[1] - thumb_tip[1])
-                print('Left click, :', distance)
                 if distance < click_threshold:
+                    print('Left click, :', distance)
                     pyautogui.click()
                     pyautogui.sleep(click_delay)
 
@@ -78,6 +85,14 @@ while True:
                     print('Right click')
                     pyautogui.rightClick()
                     pyautogui.sleep(click_delay)
+
+            #scroll down if pinky and thumb pinch
+            if pinky_tip and thumb_tip:
+                distance = abs(pinky_tip[1] - thumb_tip[1])
+                if distance < click_threshold:
+                    print("scrolling")
+                    pyautogui.scroll(-1)
+                    pyautogui.sleep(0.3)
 
 
     cv2.imshow('frame', frame)
